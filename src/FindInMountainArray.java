d230bb89103d89bd98453a9215ce4c0430f9e029
Kanishka <Kaniska.Rao@ceridian.com>
2025-04-08 18:31:02 +0530

    Binary search related questions.

---
+++ src/FindInMountainArray.java
@@ -0,0 +1,69 @@
+public class FindInMountainArray {
+    public static void main(String[] args) {
+        int[] arr = {6,7,8,9,5,4,3,2,1};
+        int target = 1;
+        /*find the peak element
+        *   - compare target with it.
+        *   - if target<peak then bS in left else right*/
+        int peak = findPeak(arr);
+        System.out.println("peak is: "+arr[peak]);
+        int start = 0;
+        int end = arr.length-1;
+        //since its peak - both left and right side will be lesser than this
+        // we will first search in the left side - if not found than in the right side.
+        int lIndex = bs(arr, target, start, peak);
+        if (lIndex != -1) {
+            System.out.println("Target found in left at: " + lIndex);
+        } else {
+            int rIndex = bs(arr, target, peak + 1, end);
+            if (rIndex != -1) {
+                System.out.println("Target found in right at: " + rIndex);
+            } else {
+                System.out.println("Target not found");
+            }
+        }
+
+    }
+
+    private static int bs(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            if (isAsc) {
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (arr[mid] < target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1; // target not found
+    }
+
+
+    private static int findPeak(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        int mid = 0;
+        while (start < end) {
+            mid = (start + end)/2;
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
--- src/FindPosInInfArray.java
+++ src/FindPosInInfArray.java
@@ -1,46 +1,60 @@
+import java.util.Arrays;
+
 public class FindPosInInfArray {
     public static void main(String[] args) {
         //assume you have given infinite array - meaning we don't know the length.
         int[] arr = {3,5,7,9,13,14,15};
         //given a target
-        int target = 14;
-        int[] range = {-1,-1};
+        int target = 13;
+        int[] range = new int[2];
 
-        //need to find the index of this target in this array
-        //since the lenght is not given, we need to find it using chunks
-        //we will start with start index as 0 and end index as 1
-        // we will see if the target falls in btw these 2 index
-        //if yes - we will apply binary search here and will find the exact index
-        //if not - we will move to the next range - will increase start position by adding 1 to the end  and will increase end position by multiplying 2 with end
-        // will apply binary search here now in this range
-        // we will check again for the target in the range and follow the above method to find out
-        //if the target is there we will find it by binary search, if not we will return -1 at the end.
-
-
-        // pattern
-        //increasing the start and end
-        //checking the target falls in this range
-        //applying binary search
-
-        int[] r = findtheRange(arr, target, range);
+        int[] r = findTheRange(arr, target, range);
         if (r[0] != -1) {
             //apply binary search here
+           int index = binarySearch(arr, target, r[0], r[1]);
+            if (index != -1) {
+                System.out.println(target + " found at index: " + index);
+            } else {
+                System.out.println("Target not found in the range");
+            }
         } else {
-            System.out.println("target not found");
+            System.out.println("Target is outside array bounds");
         }
     }
 
-    private static int[]  findtheRange(int[] arr, int target, int[] range) {
+    private static int binarySearch(int[] arr, int target, int s, int e) {
+        while (s <= e) {
+            int m = (s+e)/2;
+            if (arr[m] > target) {
+                e = m - 1;
+            } else if (arr[m] < target) {
+                s = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    private static int[] findTheRange(int[] arr, int target, int[] range) {
         int start = 0;
         int end =  1;
-        while (!(target > arr[end] && target < arr[start])) {
-            start = end + 1;
-            end = end + ((end-start)+1)*2;
+        try {
+            while (target > arr[end]) {
+                int newStart = end + 1;
+                end = end + ((end-start)+1)*2;
+                start = newStart;
+            }
+        } catch (Exception e) {
+            range[0] = -1;
+            range[1] = -1;
+            return range;
         }
+
         System.out.println("found the range");
         range[0] = start;
         range[1] = end;
+        System.out.println(Arrays.toString(range));
         return range;
         }
-
 }
